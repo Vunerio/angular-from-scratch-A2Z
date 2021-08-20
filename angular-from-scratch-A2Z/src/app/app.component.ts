c5428ddb965668d3c12e0688c3ff90170a744999
Vunerio <vunerio@gmail.com>
2021-08-20 18:43:03 +0200

    feat(lastUpdate): Asynchronous Pipe. Display the data, once it has been recovered.

--- angular-from-scratch-A2Z/src/app/app.component.ts
+++ angular-from-scratch-A2Z/src/app/app.component.ts
@@ -10,7 +10,17 @@ export class AppComponent {
 
   isAuth = false;
 
-  lastUpdate = new Date ();
+  // The async pipe subscribes to an Observable or Promise and returns the latest value it has emitted.
+  lastUpdate = new Promise(
+    (resolve, reject) => {
+      const date = new Date();
+      setTimeout(
+        () => {
+          resolve(date.getFullYear() + "/" + date.getMonth() + "/" + date.getDay() + ", " + date.getHours() + ":" + date.getMinutes());
+        }, 2000
+      );
+    }
+  );
 
   appareils = [
     {
