42757711ada58ebe397f0febfd2f278c8e0436ab
Vunerio <vunerio@gmail.com>
2021-12-05 18:33:33 +0100

    feat(auth): NavBar detect when the route changes.

--- angular-from-scratch-A2Z/src/app/app.component.ts
+++ angular-from-scratch-A2Z/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router, Event, NavigationStart } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,16 +8,27 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 
-  currentNavSelected = 1
+  currentNavSelected?: number;
   
-  constructeur() {}
+  constructor(private router: Router) {
+
+    this.router.events.subscribe((event: Event) => {
+      if (event instanceof NavigationStart) {
+        
+        if (event.url === "/path"){
+          this.currentNavSelected = 2;
+        } else {
+          this.currentNavSelected = 1;
+        }
+      }
+    });
+  }
 
   isNavSelected(navId: number): boolean {
     return this.currentNavSelected === navId;
   }
 
   selectNav(navId: number): void {
-    console.log(this.currentNavSelected, navId);
     this.currentNavSelected = navId;
   }
 }
